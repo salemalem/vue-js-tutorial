0a2201b9f3abcd9302ba9dc43cce758995ef74e9
Shyngys Shynbolatov <42411937+salemalem@users.noreply.github.com>
2019-05-14 11:50:09 +0600

    lesson 9 | Conditionals ( v-if & v-show )
    Difference between v-if and v-show:
            v-if manipulates dom. Deletes if false or inserts if true.
            If you put v-else-if first it will be error. 
            Because any if you putted above.
    
            v-show manipulates style. It just puts style="display=none"
            then element will be hidden on html page.
    
            v-show don't have any v-else-show. 
            Just shows if true.

---
+++ app.js
@@ -0,0 +1,27 @@
+/* 	@lesson 9 | Conditionals ( v-if & v-show )
+ *  @Author: Shyngys Shynbolatov | wymedia@protonmail.com
+ *  @Code from Github: https://github.com/salemalem/vue-js-tutorial
+ *  @Site on github free hosting: https://salemalem.github.io/vue-js-tutorial/
+ */  
+// https://ru.vuejs.org/v2/guide/computed.html
+
+let app = new Vue({
+	el: '#app', //Which element
+    data: {
+    	error: false,
+        success: false,
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
+
+/* PUT anything what you want into data
+ * In the example above I use message and name variables.
+ * You can name your new variables and put them your value
+ *---------------------------------------------------------
+ * Good luck :)
+ */
