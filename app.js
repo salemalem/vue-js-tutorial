a94980e8ad304da87c04a882051c641383f33700
Shyngys Shynbolatov <42411937+salemalem@users.noreply.github.com>
2019-05-14 11:29:14 +0600

    lesson 3 | Events (ex-onclick, onmousemove)
    you can use on:click & on:dblclick for double clickes.
            then you don't need to use brackets if you don't have any arguments.
            Vue.js automatically understands that.
            But if you need just pass arguments
    
            ---------------------------------------------------------        
            
            if you don't pass arguments when you call the event arguments 
            passes automatically which shown on mousemove example with canvas.
            It passed event to our method. Then we parsed (got) offsetX & offsetY
            from this MouseEvent
    
            I get this MouseEvent by simple console.log(event)
    
            ---------------------------------------------------------
    
            As v-bind: is equal to :
            v-on:click is equal to @click (means onlclick, at click)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* 	@lesson 1 | Data & methods
+/* 	@lesson 3 | Events (ex: onclick, onmousemove)
  *  @Author: Shyngys Shynbolatov | wymedia@protonmail.com
  *  @Code from Github: https://github.com/salemalem/vue-js-tutorial
  *  @Site on github free hosting: https://salemalem.github.io/vue-js-tutorial/
@@ -7,15 +7,22 @@
 let app = new Vue({
     el: '#app', //Which element
     data: { //you can pass any variables into the data
-    	message: 'Hello World!',
-    	name: 'Shyngys'
+    	age: 17, 
+    	// div.canvas
+    	x: 0,
+    	y: 0,
     },
-    methods: { //work with data
-    	greet: function(time){
-    		return 'Good '+time
+    methods:{
+    	add: function(inc){ //increment
+    		this.age+=inc;
     	},
-    	iam: function(){
-    		return 'Hello my name is '+this.name
+    	subtract: function(dec){ //decrement
+			this.age-=dec;
+    	},
+    	// div.canvas
+    	updateXY: function(event){//ne need to call with updateXY(event). It passes auto.
+    		this.x = event.offsetX;
+    		this.y = event.offsetY;
     	}
     }
 });
@@ -24,9 +31,52 @@ let app = new Vue({
  * In the example above I use message and name variables.
  * You can name your new variables and put them your value
  *---------------------------------------------------------
- * greet method takes only time for argument and displays 
- * with Good in the beginning.
- *---------------------------------------------------------
- * iam method as you see works with data.
- * To access data simply add this. before your variable name
+ * When we console.log(event) which in updateXY
+ * we got this output in console.
+ * MouseEvent
+ * {
+ *		altKey: false
+ * 		bubbles: true
+ *		button: 0
+ *		buttons: 0
+ *		cancelBubble: false
+ *		cancelable: true
+ *		clientX: 630
+ *		clientY: 340
+ *		composed: true
+ *		ctrlKey: false
+ *		currentTarget: null
+ *		defaultPrevented: false
+ *		detail: 0
+ *		eventPhase: 0
+ *		fromElement: null
+ *		isTrusted: true
+ *		layerX: 630
+ *		layerY: 835
+ *		metaKey: false
+ *		movementX: 27
+ *		movementY: -5
+ *		offsetX: 621
+ *		offsetY: -266
+ *		pageX: 630
+ *		pageY: 835
+ *		path: (6) [div.canvas, div#app, body, html, document, Window]
+ *		relatedTarget: null
+ *		returnValue: true
+ *		screenX: 630
+ *		screenY: 411
+ *		shiftKey: false
+ *		sourceCapabilities: InputDeviceCapabilities {firesTouchEvents: false}
+ *		srcElement: div.canvas
+ *		target: div.canvas
+ *		timeStamp: 11521.999999999935
+ *		toElement: div.canvas
+ *		type: "mousemove"
+ *		view: Window {postMessage: ƒ, blur: ƒ, focus: ƒ, close: ƒ, parent: Window, …}
+ *		which: 0
+ *		x: 630
+ *		y: 340
+ *  }
+ * to get mouse coordinates we use offsetX & offsetY
+ * place this value into this.x = offsetX & for y
  */
