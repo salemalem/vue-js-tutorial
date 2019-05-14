5cfd4553216bdf07fe278eb1805ddf7db8025482
Shyngys Shynbolatov <42411937+salemalem@users.noreply.github.com>
2019-05-14 11:10:20 +0600

    lesson 0 | Creating Vue instance
    TO SHOW your variables which are defined in your script
            you can use curly braces.
            {{  }}
    
            Insert your variable name, then browser will render and 
            show value of variable.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* 	@lesson 1 | Data & methods
+/* 	@lesson 0 | Creating Vue instance
  *  @Author: Shyngys Shynbolatov | wymedia@protonmail.com
  *  @Code from Github: https://github.com/salemalem/vue-js-tutorial
  *  @Site on github free hosting: https://salemalem.github.io/vue-js-tutorial/
@@ -6,27 +6,13 @@
 
 let app = new Vue({
     el: '#app', //Which element
-    data: { //you can pass any variables into the data
+    data: {
     	message: 'Hello World!',
     	name: 'Shyngys'
-    },
-    methods: { //work with data
-    	greet: function(time){
-    		return 'Good '+time
-    	},
-    	iam: function(){
-    		return 'Hello my name is '+this.name
-    	}
     }
 });
 
 /* PUT anything what you want into data
  * In the example above I use message and name variables.
  * You can name your new variables and put them your value
- *---------------------------------------------------------
- * greet method takes only time for argument and displays 
- * with Good in the beginning.
- *---------------------------------------------------------
- * iam method as you see works with data.
- * To access data simply add this. before your variable name
  */
