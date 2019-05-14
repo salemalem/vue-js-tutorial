75f8b8592902fccf95503a836cd77d93f6fcf597
Shyngys Shynbolatov <42411937+salemalem@users.noreply.github.com>
2019-05-14 14:10:39 +0600

    lesson 10 | Looping with v-for | Cycles
    any while. just for.
            {{ frameworks[0] }}  
            This will display { "name": "Vue.js", "cool_level": 99 }
            But what if we display all elements of frameworks array.
            
            --------------------------------------------------------------
    
            There are 2 ways to display framework of frameworks.
            1) <li v-for="framework in frameworks" v-text="framework.name"></li>
            or if it contain html v-html
            2) <li v-for="framework in frameworks">{{framework.name}}</li>
    
            --------------------------------------------------------------
    
            if you now instead of in you can write of.
            for ex find difference:
            1) v-for="framework in frameworks"
            2) v-for="framework of frameworks"
    
            --------------------------------------------------------------
    
            also type what you want instead of framework. for ex: foo
            but I use work framework to make my code readable.
    
            --------------------------------------------------------------
    
            Why framework.name?
            Just using framework will give us {name: 'Vue.js',     cool_level: 99},
            output.
            to access each parameter name write your object name
            framework.parameter_name
    
            --------------------------------------------------------------
    
            In the character case I used just character. Because in js file I
            defined characters as ['Jessica', 'Stella', 'Emily', 'Ray'].
            And frameworks has 
            {name: 'Vue.js',     cool_level: 99},
            {name: 'React.js',   cool_level: 88}, //Yeah Vue.js is cooler))
            {name: 'Angular.js', cool_level: 77},
    
            Feel the difference. frameworks wrapped into json.
    
            --------------------------------------------------------------
    
            Look at the third example. 
            Name: Sven printed like:
            S
            v
            e
            n
    
            It means if this is one element it will be divided to array then printed.
    
            --------------------------------------------------------------
    
            I used frameworks in frameworks WHY?
            If you know first frameworks respond to be printed, While the second one 
            array name. They won't be confilcted. But try to make your code readable.
            
            --------------------------------------------------------------
    
            Did you noticed i or index?
            You can put like (frameworks, index) or (frameworks, i) in frameworks.
            Put your index value of array, then index of element.
    
            TRY TO CODE READABLE!!!

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
+/* 	@lesson 10 | Looping with v-for | Cycles
+ *  @Author: Shyngys Shynbolatov | wymedia@protonmail.com
+ *  @Code from Github: https://github.com/salemalem/vue-js-tutorial
+ *  @Site on github free hosting: https://salemalem.github.io/vue-js-tutorial/
+ */  
+// https://vuejs.org/v2/guide/list.html
+
 let app = new Vue({
-    el: '#app',
+	el: '#app', //Which element
+    data: {
+    	name: 'Sven',
+    	characters: ['Jessica', 'Stella', 'Emily', 'Ray'],
+    	frameworks:[
+    		{name: 'Vue.js',     cool_level: 99},
+    		{name: 'React.js',   cool_level: 88}, //Yeah Vue.js is cooler))
+    		{name: 'Angular.js', cool_level: 77},
+    	],
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
 });
 
+/* PUT anything what you want into data
+ * In the example above I use message and name variables.
+ * You can name your new variables and put them your value
+ *---------------------------------------------------------
+ * Good luck :)
+ */
