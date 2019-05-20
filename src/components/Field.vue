38cfe2006101b2dc1269782491f776db0df71820
Shyngys Shynbolatov <42411937+salemalem@users.noreply.github.com>
2019-05-20 09:21:28 +0600

    lesson 16 | Connection between components
    Binding each component with each other

---
+++ src/components/Field.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h2>{{ message }}</h2>
+		<input type="text" :value="message" @input="changeMess">
+		{{ mess }}
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['message'],
+		data(){
+			return {
+				mess: 'Hello, Example'
+			}
+		},
+		methods:{
+			changeMess(event){
+				this.mess = event.target.value;
+				this.$emit('messChange', this.mess);
+			}
+		}
+	}
+</script>
---
+++ src/components/Submitbtn.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app-submitbtn">
+		<button type="submit" id="submit">{{mess}}</button>
+	</div>
+</template>
+
+<script>
+	import Field from './Field.vue';
+	let mess = Field.data().mess;
+	
+	export default {
+		data(){
+			return {
+				mess: mess
+			}
+		}
+	}
+</script>
+
+<style>
+	button[type=submit]{
+		size: 50px;
+		background: red;
+	}
+</style>
